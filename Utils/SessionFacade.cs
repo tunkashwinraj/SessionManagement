132d1d6e9a0a275b82fe79e4944e85684f01a264
Ashwin Raj Tunk <84958360+tunkashwinraj@users.noreply.github.com>
2024-12-14 12:54:21 -0500

    Add files via upload

---
+++ Utils/SessionFacade.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Web;
+
+namespace SessionManagement_1262774.Utils
+{
+    public class SessionFacade
+    {
+        private SessionFacade() { }
+
+        // Declare session constants for the key names
+        private const string USERNAME = "USERNAME";
+        private const string ACCOUNTNUM = "ACCOUNTNUM";
+        private const string LASTLOGIN = "LASTLOGIN";
+
+        public static string UserName
+        {
+            get => HttpContext.Current.Session[USERNAME]?.ToString();
+            set => HttpContext.Current.Session[USERNAME] = value;
+        }
+
+        public static int? Account
+        {
+            get => HttpContext.Current.Session[ACCOUNTNUM] as int?;
+            set => HttpContext.Current.Session[ACCOUNTNUM] = value;
+        }
+
+        public static DateTime? LastLogin
+        {
+            get => HttpContext.Current.Session[LASTLOGIN] as DateTime?;
+            set => HttpContext.Current.Session[LASTLOGIN] = value;
+        }
+    }
+}
